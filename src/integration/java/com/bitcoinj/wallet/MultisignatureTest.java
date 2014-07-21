05355965e38260adcfdf7ac5c4b96979f19607a1
Jiri Peinlich <jiri.peinlich@gmail.com>
2014-07-21 08:30:45 +0200

    extracted working with wallet into separate test class

--- src/integration/java/com/bitcoinj/wallet/MultisignatureTest.java
+++ src/integration/java/com/bitcoinj/wallet/MultisignatureTest.java
@@ -2,22 +2,17 @@ package com.bitcoinj.wallet;
 
 import com.google.bitcoin.core.*;
 import com.google.bitcoin.crypto.TransactionSignature;
-import com.google.bitcoin.kits.WalletAppKit;
-import com.google.bitcoin.params.TestNet3Params;
 import com.google.bitcoin.script.Script;
 import com.google.bitcoin.script.ScriptBuilder;
 import com.google.common.util.concurrent.ListenableFuture;
-import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.BrokenBarrierException;
-import java.util.concurrent.CyclicBarrier;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -28,42 +23,8 @@ import static org.hamcrest.Matchers.is;
  * <p/>
  * Created by Jiri on 21. 7. 2014.
  */
-public class MultisignatureTest {
+public class MultisignatureTest  extends WalletTests{
     private static final Logger logger = LoggerFactory.getLogger(MultisignatureTest.class);
-
-    private final File workingDirectory = new File("build/work");
-    private final NetworkParameters networkParameters = TestNet3Params.get();
-    private WalletAppKit walletAppKit;
-    private Wallet wallet;
-
-
-    @Before
-    public void createWorkingDir() {
-        if (!workingDirectory.exists()) {
-            if (!workingDirectory.mkdir()) {
-                throw new RuntimeException("Could not create named directory.");
-            }
-        }
-    }
-
-    @Before
-    public void initWallet() {
-        walletAppKit = new WalletAppKit(networkParameters, workingDirectory, "Secure-btc-change-me") {
-            @Override
-            protected void onSetupCompleted() {
-                // This is called in a background thread after startAndWait is called, as setting up various objects
-                // can do disk and network IO that may cause UI jank/stuttering in wallet apps if it were to be done
-                // on the main thread.
-                while ((wallet().getImportedKeys().size() < 1)) {
-                    wallet().importKey(new ECKey());
-                }
-            }
-        };
-        walletAppKit.startAsync();
-        walletAppKit.awaitRunning();
-        wallet = walletAppKit.wallet();
-    }
-
 
     /**
      * this method asks BitcoinJ to create multisignature redeem script. It will be m/n redeem script where m is
@@ -100,46 +61,6 @@ public class MultisignatureTest {
     }
 
     /**
-     * This method will send amount provided to address provided.
-     */
-    private Transaction sendMoneyToMultisigAddress(Coin amountToSend, String address) throws AddressFormatException, InsufficientMoneyException, InterruptedException, BrokenBarrierException {
-        Address multiSignatureAddress = new Address(networkParameters, address);
-        final Wallet.SendResult sendResult = wallet.sendCoins(walletAppKit.peerGroup(), multiSignatureAddress, amountToSend);
-
-        logger.info("Trying to send to address {}", address);
-        ListenableFuture<Transaction> future = sendResult.broadcastComplete;
-        waitForFuture(future, "Money is send, you can try to spend it");
-        return sendResult.tx;
-    }
-
-    /**
-     * method waiting for broadcast to complete.
-     */
-    private void waitForFuture(ListenableFuture<Transaction> future, final String logMessage) throws InterruptedException, BrokenBarrierException {
-        final CyclicBarrier barrier = new CyclicBarrier(2);
-        future.addListener(new Runnable() {
-            @Override
-            public void run() {
-                logger.info(logMessage);
-                try {
-                    barrier.await();
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        }, new SynchronousExecutor());
-        barrier.await();
-    }
-
-    /**
-     * This method broadcasts transaction to the bitcoin network
-     */
-    private void broadcastSignedTransaction(Transaction spendingTransaction) throws InterruptedException, BrokenBarrierException {
-        ListenableFuture<Transaction> transactionListenableFuture = walletAppKit.peerGroup().broadcastTransaction(spendingTransaction);
-        waitForFuture(transactionListenableFuture, "money should be on the way back.");
-    }
-
-    /**
      * This method will sign all the inputs of the transaction using the two keys provided.
      */
     private void signSpendingTransaction(ECKey key1, ECKey key2, String multiSignatureRedeemScript, Transaction spendingTransaction) {
@@ -166,17 +87,18 @@ public class MultisignatureTest {
      *
      */
     private Transaction createSpendingTransaction(List<TransactionOutput> watchedOutputs) {
-        Transaction transaction = new Transaction(networkParameters);
+        Transaction transaction = new Transaction(params());
 
         Coin total = Coin.ZERO;
         for (TransactionOutput watchedOutput : watchedOutputs) {
             transaction.addInput(watchedOutput);
             total = total.add(watchedOutput.getValue());
         }
-        transaction.addOutput(total.subtract(Transaction.REFERENCE_DEFAULT_MIN_TX_FEE), wallet.freshReceiveAddress());
+        transaction.addOutput(total.subtract(Transaction.REFERENCE_DEFAULT_MIN_TX_FEE), freshAddress());
 
         return transaction;
     }
+
 
 
     @Test
@@ -197,39 +119,28 @@ public class MultisignatureTest {
         //we will create P2SH address
         String p2shAddress = getAddressFromRedeemScript(multiSignatureRedeemScript);
 
-        Address address = new Address(networkParameters, p2shAddress);
-        //TODO: try to watch the multisign address for incoming money
+        Address address = new Address(params(), p2shAddress);
 
-        wallet.addWatchedAddress(address);
+        watchAddress(address);
 
 
         //We will now try to send money to the multisign address
         Transaction depositTransaction;
         try {
-            depositTransaction = sendMoneyToMultisigAddress(Transaction.REFERENCE_DEFAULT_MIN_TX_FEE.multiply(3), p2shAddress);
+            depositTransaction = sendMoneyToAddress(Transaction.REFERENCE_DEFAULT_MIN_TX_FEE.multiply(3), p2shAddress);
         } catch (com.google.bitcoin.core.InsufficientMoneyException e) {
-            logger.error("Your balance is: {}", wallet.getBalance());
-            logger.error("Send faucet money to this address {}", wallet.getImportedKeys().get(0).toAddress(networkParameters));
-            logger.error("Send faucet money to this address {}", wallet.freshReceiveAddress());
+            logger.error("Your balance is: {}", getBalance());
+            logger.error("Send faucet money to this address {}", freshAddress());
             logger.error("Try this faucet http://faucet.xeno-genesis.com/");
             logger.error("Or Try this faucet https://tpfaucet.appspot.com/");
             throw e;
         }
 
         //lets get all the outputs that we are watching
-        List<TransactionOutput> watchedOutputs = wallet.getWatchedOutputs(false);
-        List<TransactionOutput> filteredOutputs = new LinkedList<>();
-
-        //filter only the outputs that are for the address that we created in this test
-        for (TransactionOutput watchedOutput : watchedOutputs) {
-            Address toAddress = watchedOutput.getScriptPubKey().getToAddress(networkParameters);
-            if (toAddress.equals(address)) {
-                filteredOutputs.add(watchedOutput);
-            }
-        }
+        List<TransactionOutput> watchedOutputs = getWatchedOutputs(address);
 
         //create ne transaction to send money back to the wallet
-        Transaction spendingTransaction = createSpendingTransaction(filteredOutputs);
+        Transaction spendingTransaction = createSpendingTransaction(watchedOutputs);
 
         //sign the spending transaction using keys created
         signSpendingTransaction(key1, key2, multiSignatureRedeemScript, spendingTransaction);
@@ -237,19 +148,10 @@ public class MultisignatureTest {
         //broadcast the transaction
         broadcastSignedTransaction(spendingTransaction);
 
-        assertThat(1,is(2));
     }
 
-    @Test
-    public void showBalance() {
-        logger.info(wallet.getBalance().toFriendlyString());
-    }
 
-    private class SynchronousExecutor implements java.util.concurrent.Executor {
-        @Override
-        public void execute(Runnable command) {
-            command.run();
-        }
-    }
+
+
 
 }
---
+++ src/integration/java/com/bitcoinj/wallet/WalletTests.java
@@ -0,0 +1,140 @@
+package com.bitcoinj.wallet;
+
+import com.google.bitcoin.core.*;
+import com.google.bitcoin.kits.WalletAppKit;
+import com.google.bitcoin.params.TestNet3Params;
+import com.google.common.util.concurrent.ListenableFuture;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+/**
+ * Created by Jiri on 21. 7. 2014.
+ */
+public class WalletTests {
+    private static final Logger logger = LoggerFactory.getLogger(WalletTests.class);
+
+
+    private final File workingDirectory = new File("build/work");
+    private final NetworkParameters networkParameters = TestNet3Params.get();
+
+    private WalletAppKit walletAppKit;
+    private Wallet wallet;
+
+
+    @Before
+    public void createWorkingDir() {
+        if (!workingDirectory.exists()) {
+            if (!workingDirectory.mkdir()) {
+                throw new RuntimeException("Could not create named directory.");
+            }
+        }
+    }
+
+    @Before
+    public void initWallet() {
+        walletAppKit = new WalletAppKit(networkParameters, workingDirectory, "Secure-btc-change-me") {
+            @Override
+            protected void onSetupCompleted() {
+                // This is called in a background thread after startAndWait is called, as setting up various objects
+                // can do disk and network IO that may cause UI jank/stuttering in wallet apps if it were to be done
+                // on the main thread.
+                while ((wallet().getImportedKeys().size() < 1)) {
+                    wallet().importKey(new ECKey());
+                }
+            }
+        };
+        walletAppKit.startAsync();
+        walletAppKit.awaitRunning();
+        wallet = walletAppKit.wallet();
+    }
+    /**
+     * This method will send amount provided to address provided. It can be P2SH address.
+     */
+    protected Transaction sendMoneyToAddress(Coin amountToSend, String address) throws AddressFormatException, InsufficientMoneyException, InterruptedException, BrokenBarrierException {
+        Address multiSignatureAddress = new Address(networkParameters, address);
+        final Wallet.SendResult sendResult = wallet.sendCoins(walletAppKit.peerGroup(), multiSignatureAddress, amountToSend);
+
+        logger.info("Trying to send to address {}", address);
+        ListenableFuture<Transaction> future = sendResult.broadcastComplete;
+        waitForFuture(future, "Money is send, you can try to spend it");
+        return sendResult.tx;
+    }
+
+    /**
+     * method waiting for broadcast to complete.
+     */
+    private void waitForFuture(ListenableFuture<Transaction> future, final String logMessage) throws InterruptedException, BrokenBarrierException {
+        final CyclicBarrier barrier = new CyclicBarrier(2);
+        future.addListener(new Runnable() {
+            @Override
+            public void run() {
+                logger.info(logMessage);
+                try {
+                    barrier.await();
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }, new SynchronousExecutor());
+        barrier.await();
+    }
+
+    protected NetworkParameters params() {
+        return networkParameters;
+    }
+
+    /**
+     * This method broadcasts transaction to the bitcoin network
+     */
+    protected void broadcastSignedTransaction(Transaction spendingTransaction) throws InterruptedException, BrokenBarrierException {
+        ListenableFuture<Transaction> transactionListenableFuture = walletAppKit.peerGroup().broadcastTransaction(spendingTransaction);
+        waitForFuture(transactionListenableFuture, "money should be on the way back.");
+    }
+
+    protected Address freshAddress() {
+        return wallet.freshReceiveAddress();
+    }
+
+    protected void watchAddress(Address address) {
+        wallet.addWatchedAddress(address);
+    }
+
+    protected Coin getBalance(){
+        return wallet.getBalance();
+    }
+    protected List<TransactionOutput> getWatchedOutputs(Address address) {
+        List<TransactionOutput> watchedOutputs = wallet.getWatchedOutputs(false);
+        List<TransactionOutput> filteredOutputs = new LinkedList<>();
+
+        //filter only the outputs that are for the address that we created in this test
+        for (TransactionOutput watchedOutput : watchedOutputs) {
+            Address toAddress = watchedOutput.getScriptPubKey().getToAddress(networkParameters);
+            if (toAddress.equals(address)) {
+                filteredOutputs.add(watchedOutput);
+            }
+        }
+        return filteredOutputs;
+    }
+
+    private class SynchronousExecutor implements java.util.concurrent.Executor {
+        @Override
+        public void execute(Runnable command) {
+            command.run();
+        }
+    }
+
+    @Test
+    public void showBalance() {
+        logger.info(getBalance().toFriendlyString());
+    }
+
+
+}
